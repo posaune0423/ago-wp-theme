1eedec311758bccd0df40ff06582c11edfb04b79
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-14 04:02:31 +0900

    feat: create components for top page

---
+++ ago-theme/src/components/Top/ConcertList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Topページ用コンサートリスト</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopConcertList'
+};
+</script>
+
+<style scoped></style>
---
+++ ago-theme/src/components/Top/NewsList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <h1>
+          Topページ用News List
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
