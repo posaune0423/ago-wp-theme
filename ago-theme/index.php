dbb49a6a60baa0e8080e8688a8a8e7e1c086d592
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-14 16:51:24 +0900

    feat: add link to google font

--- ago-theme/index.php
+++ ago-theme/index.php
@@ -5,6 +5,9 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <meta name="theme-color" content="#42b983">
     <?php if (is_front_page()) : ?>
         <meta name="description" content="<?php bloginfo('description'); ?>">
