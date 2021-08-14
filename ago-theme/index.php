b06632b4f539a2917682c043b20a6485b777f6ed
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-15 05:54:38 +0900

    set icon

--- ago-theme/index.php
+++ ago-theme/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="icon" href="/wp-content/themes/ago-theme/static/logo.jpg">
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
