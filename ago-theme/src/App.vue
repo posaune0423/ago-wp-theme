2b4501cada4dea0c8c2a9d18565aead339ebc1bc
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-14 16:51:51 +0900

    fix: modify font family

--- ago-theme/src/App.vue
+++ ago-theme/src/App.vue
@@ -25,11 +25,9 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', 'Noto Sans JP', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
