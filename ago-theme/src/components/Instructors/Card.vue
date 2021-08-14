02e9095682da2dd7f308264d96a168bd1865d4a3
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-15 01:11:32 +0900

    feat: create instructor page

---
+++ ago-theme/src/components/Instructors/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mx-auto" width="300">
+    <v-img :src="image" height="280px" :position="position || 'center'"></v-img>
+
+    <v-card-title>
+      {{ name }}
+    </v-card-title>
+
+    <v-card-subtitle> {{ belonging }} | {{ part }} </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn icon class="align-end" @click="show = !show">
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{ description }}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['name', 'image', 'description', 'part', 'belonging', 'position'],
+  data: () => ({
+    show: false
+  })
+};
+</script>
+
+<style lang="scss" scoped></style>
