65700e56493cdf6368445d7a85bc964205316eb6
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-16 01:56:47 +0900

    fix: modify head

--- ago-theme/index.php
+++ ago-theme/index.php
@@ -3,18 +3,14 @@
 
 <head>
     <title>青山学院管弦楽団</title>
-    <link rel="icon" href="/wp-content/themes/ago-theme/static/logo.png">
-    <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="/wp-content/themes/ago-theme/static/logo.png">
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="/wp-content/themes/ago-theme/dist/css/style.css" as="style" onload="this.rel='stylesheet'">
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php if (is_front_page()) : ?>
-        <meta name="description" content="<?php bloginfo('description'); ?>">
-    <?php endif; ?>
     <?php wp_head(); ?>
 </head>
 
@@ -24,9 +20,9 @@
 
 
     <!-- CDN Vue related library so that reduce bundle file -->
-    <script defer src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-    <script defer type="text/javascript" src="/wp-content/themes/ago-theme/dist/js/bundle.js"></script>
+    <script defer type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+    <script defer type="text/javascript" src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+    <script defer async type="text/javascript" src="/wp-content/themes/ago-theme/dist/js/bundle.js"></script>
 </body>
 
 </html>
