ac50d243418834d8839a2c83334387c4767b77ba
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-15 18:15:05 +0900

    feat: create pager component

--- ago-theme/src/components/News/List.vue
+++ ago-theme/src/components/News/List.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
-    <div class="container_post">
+    <div class="newsList__container">
       <post-card v-for="post in posts" :key="post.id" :post="post" />
     </div>
-    <div class="text-center mt-6">
-      <div class="pagination text-center">
-        <v-btn :disabled="!prev" @click="$router.push(`/news/?page=${prev}`)">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-        <v-btn :disabled="!next" @click="$router.push(`/news/?page=${next}`)">
-          <v-icon>mdi-chevron-right</v-icon>
-        </v-btn>
-      </div>
-    </div>
+    <pager :next="next" :prev="prev" />
   </div>
 </template>
 
 <script>
 import PostCard from '@/components/News/Card';
+import Pager from '@/components/molecules/Pager';
 
 export default {
   name: 'PostList',
   components: {
-    PostCard
+    PostCard,
+    Pager
   },
   data() {
     return {
@@ -35,7 +28,7 @@ export default {
   },
   methods: {
     getPosts(page = 1) {
-      this.$axios.get(`wp/v2/posts?per_page=2&page=${page}`).then((res) => {
+      this.$axios.get(`wp/v2/posts?per_page=3&page=${page}`).then((res) => {
         this.current = page;
         this.posts = res.data;
         this.makePagination(res);
@@ -69,7 +62,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container_post {
+.newsList__container {
   flex-wrap: wrap;
   display: flex;
   gap: 2rem;
---
+++ ago-theme/src/components/molecules/Pager.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center mt-6">
+    <div class="pagination text-center">
+      <v-btn :disabled="!prev" @click="$router.push(`/news/?page=${prev}`)">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn :disabled="!next" @click="$router.push(`/news/?page=${next}`)">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pager',
+  props: ['prev', 'next']
+};
+</script>
+
+<style lang="scss" scoped></style>
