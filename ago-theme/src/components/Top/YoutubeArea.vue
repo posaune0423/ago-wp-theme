cc3d95db729d560852719847e7eca7b5f0b8fd0a
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-15 14:10:05 +0900

    feat: create Youtube area

--- ago-theme/src/components/Top/YoutubeArea.vue
+++ ago-theme/src/components/Top/YoutubeArea.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="movie">
-    <div class="movie-frame">
+  <div class="movie_container">
+    <div class="movie_frame">
       <iframe
-        src="https://www.youtube.com/embed/f_41P8qOJUY"
+        src="https://www.youtube.com/embed/vJpIiOI4UYw"
         frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
-      >
-      </iframe>
+      ></iframe>
     </div>
   </div>
 </template>
@@ -16,39 +15,38 @@
 export default {};
 </script>
 
-<style>
-/* Movie */
-.movie {
+<style lang="scss" scoped>
+.movie_container {
   margin-bottom: 40px;
 }
-.movie-frame {
+.movie_frame {
   position: relative;
   height: 0;
   margin: 0 auto;
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 }
-.movie-frame iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-@media (min-width: 970px) {
-  .movie {
+@include pc-screen() {
+  .movie_container {
     padding: 50px;
     margin-right: 20px;
     margin-left: 20px;
   }
-  .movie-frame {
+  .movie_frame {
     width: 80%;
     padding-top: 40%;
   }
 }
-@media (max-width: 970px) {
-  .movie {
+@include sp-screen() {
+  .movie_container {
     margin-bottom: 40px;
   }
-  .movie-frame {
+  .movie_frame {
     width: 100%;
     padding-top: 60%;
   }
