eb9ce7c32adef2b934a9f8964ac1f947043da60f
Asuma Yamada <yamadaasuma@gmail.com>
2021-08-15 15:24:47 +0900

    fix: modify css

--- ago-theme/src/components/Top/HeroArea.vue
+++ ago-theme/src/components/Top/HeroArea.vue
@@ -18,89 +18,105 @@
 export default {};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 #bg-video {
   min-width: 100%;
   min-height: 100%;
   width: 100%;
   background-size: cover;
 }
-.video-wrap h1 {
-  font-weight: 900;
-  padding: 0 27px;
-}
-.video-wrap p {
-  font-weight: bold;
-  padding: 0 27px;
+.video-wrap {
+  h1 {
+    font-weight: 900;
+    padding: 0 27px;
+  }
+  p {
+    font-weight: bold;
+    padding: 0 27px;
+  }
 }
 @media (min-width: 1268px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 125px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 35px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 125px;
+    }
+    p {
+      color: #fff;
+      font-size: 35px;
+    }
   }
 }
 @media (max-width: 1268px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 100px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 28px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 100px;
+    }
+    p {
+      color: #fff;
+      font-size: 28px;
+    }
   }
 }
 @media (max-width: 1107px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 90px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 25px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 90px;
+    }
+    p {
+      color: #fff;
+      font-size: 25px;
+    }
   }
 }
 @media (max-width: 997px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 85px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 20px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 85px;
+    }
+    p {
+      color: #fff;
+      font-size: 20px;
+    }
   }
 }
 @media (max-width: 922px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 70px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 17px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 70px;
+    }
+    p {
+      color: #fff;
+      font-size: 17px;
+    }
   }
 }
 @media (max-width: 946px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 62px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 17px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 62px;
+    }
+    p {
+      color: #fff;
+      font-size: 17px;
+    }
   }
 }
 @media (max-width: 860px) {
-  .video-wrap h1 {
-    color: #fff;
-    font-size: 57px;
-  }
-  .video-wrap p {
-    color: #fff;
-    font-size: 17px;
+  .video-wrap {
+    h1 {
+      color: #fff;
+      font-size: 57px;
+    }
+    p {
+      color: #fff;
+      font-size: 17px;
+    }
   }
 }
 @media (min-width: 793px) {
@@ -111,14 +127,16 @@ export default {};
   }
 }
 @media (max-width: 793px) {
-  .video-wrap h1 {
-    padding-top: 10px;
-    color: #111;
-    font-size: 40px;
-  }
-  .video-wrap p {
-    color: #111;
-    font-size: 25px;
+  .video-wrap {
+    h1 {
+      padding-top: 10px;
+      color: #111;
+      font-size: 40px;
+    }
+    p {
+      color: #111;
+      font-size: 25px;
+    }
   }
 }
 </style>
