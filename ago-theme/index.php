4362d90b828fe3a4d87b670de588d213d3e38140
Asuma Yamada <yamadaasuma@gmail.com>
2021-09-05 15:59:37 +0900

    fix: modify header

--- ago-theme/index.php
+++ ago-theme/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <title>青山学院管弦楽団</title>
+    <title>青山学院管弦楽団 | Aoyama Gakuin Orchestra</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" href="/wp-content/themes/ago-theme/static/logo.png">
+    <link rel="icon" type="image/x-icon" href="/wp-content/themes/ago-theme/static/favicon.ico">
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap" as="style" onload="this.rel='stylesheet'" crossorigin>
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" as="style" onload="this.rel='stylesheet'" crossorigin>
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" as="style" onload="this.rel='stylesheet'" crossorigin>
     <link rel="preload" href="/wp-content/themes/ago-theme/dist/css/style.css" as="style" onload="this.rel='stylesheet'">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="青山学院管弦楽団のHPです。">
+    <meta name="description" content="青山学院管弦楽団のHPです。各種お知らせや演奏会情報を掲載しています。">
     <meta name="keywords" content="オーケストラ,青山学院,大学,オケ,青学" />
     <?php wp_head(); ?>
 </head>
